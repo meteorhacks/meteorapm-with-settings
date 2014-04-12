827c44fa0dd09bf0557bb1fc7a9608f23d0aa05a
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-04-12 21:45:19 +0530

    initial commit

---
+++ via-settings.js
@@ -0,0 +1,18 @@
+if (Meteor.isClient) {
+  Template.hello.greeting = function () {
+    return "Welcome to via-settings.";
+  };
+
+  Template.hello.events({
+    'click input': function () {
+      // template data, if any, is available in 'this'
+      if (typeof console !== 'undefined')
+        console.log("You pressed the button");
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Apm.connect(Meteor.settings.apmAppId, Meteor.settings.apmAppSecret);
+}
+
